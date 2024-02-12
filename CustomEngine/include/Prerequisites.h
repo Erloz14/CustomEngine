536c8bd923fbbf6154e6871a3fc4c53ad6cdeeba
Erloz14 <erick.lozano.martinez96@hotmail.com>
2024-02-11 19:13:52 -0600

    main- Window class
    creamos clase ventana,

---
+++ CustomEngine/include/Prerequisites.h
@@ -0,0 +1,20 @@
+#pragma once
+//std lib 
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <chrono>
+
+//external lib
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
+#include <xnamath.h>
+
+// windows
+#include <windows.h>
+
+//Inernal Includes
+ #include "Resource.h"
